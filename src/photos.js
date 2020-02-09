ae117aab4ce2e9fc2e08d2be0e1857cad129486c
Kyeongmin Kim <kkfenix3085@naver.com>
2020-02-09 19:19:52 +0900

    change background image by image files in "images" directory.

--- src/photos.js
+++ src/photos.js
@@ -1,3 +1,28 @@
-/**
- * Managing Background Images
- */
+const style = document.body.style;
+const num_images = 9;
+let images = [];
+let prevRand = 0;
+let curRand = 0;
+function changeBackground() {
+  do {
+    prevRand = parseInt(Math.random() * num_images, 10);
+  } while (prevRand === curRand);
+  curRand = prevRand;
+
+  style.backgroundImage = images[curRand];
+
+  console.log(images[curRand]);
+}
+
+function initializeImageArray() {
+  for (let i = 0; i < num_images; i++) {
+    images.push(`url("images/img${i + 1}.jpg")`);
+  }
+}
+
+function init() {
+  initializeImageArray();
+  setInterval(changeBackground, 1000);
+}
+
+// init();
